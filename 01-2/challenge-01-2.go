a8991bd6de251f97dd7b5e9e01ad7c9cfd44d3b2
Ryan Harris <harrisryan1@gmail.com>
2019-12-02 21:10:37 -0500

    Finish Day 01 Pt 02

--- 01-2/challenge-01-2.go
+++ 01-2/challenge-01-2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -35,7 +36,53 @@ func getMassesList() []int {
 	return massesIntArray
 }
 
+func calculateFuel(mass int) float64 {
+	return math.Floor(float64(mass/3)) - 2
+}
+
+func getModuleFuelRequirement(moduleMass int) float64 {
+	counter := float64(moduleMass)
+	var fuelRequirements = []float64{}
+
+	// Calculate the fuel requirement array for this module until
+	// we reach a 0 or negative value.
+	for {
+		fuel := calculateFuel(int(counter))
+		counter = fuel
+
+		if counter <= 0 {
+			break
+		}
+
+		fuelRequirements = append(fuelRequirements, fuel)
+	}
+
+	// Sum the array of fuel requirements for this given module
+	var sum float64
+	for _, req := range fuelRequirements {
+		sum += req
+	}
+
+	return sum
+}
+
 func main() {
-	masses := getMassesList()
-	fmt.Println(masses)
+	moduleRequirementsArray := []float64{}
+	moduleMasses := getMassesList()
+
+	// Iterate through the list of module masses and create an array of fuel
+	// requirements for each module with the weight of the fuel taken into account.
+	for i := 0; i < len(moduleMasses); i++ {
+		moduleFuelNeeded := getModuleFuelRequirement(moduleMasses[i])
+		moduleRequirementsArray = append(moduleRequirementsArray, moduleFuelNeeded)
+	}
+
+	// Sum the array of fuel requirements by module
+	var totalFuel float64
+	for _, moduleFuel := range moduleRequirementsArray {
+		totalFuel += moduleFuel
+	}
+
+	fmt.Println("Total fuel needed:")
+	fmt.Println(fmt.Sprintf("%.2f", totalFuel))
 }
