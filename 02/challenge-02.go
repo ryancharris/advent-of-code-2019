80abc7eba86b7116e7dc2c5b4afcb917334eca9b
Ryan Harris <harrisryan1@gmail.com>
2019-12-04 00:08:26 -0500

    Try again

--- 02/challenge-02.go
+++ 02/challenge-02.go
@@ -33,37 +33,49 @@ func readInput() []int {
 }
 
 func main() {
-	intCodes := readInput()
+	// intCodes := readInput()
+	// intCodes := [5]int{1, 0, 0, 0, 99}
+	// intCodes := [5]int{2, 3, 0, 3, 99}
+	intCodes := [6]int{2, 4, 4, 5, 99, 0}
+	// intCodes := [9]int{1, 1, 1, 4, 99, 5, 6, 0, 99}
+
+	fmt.Print("intCodes", intCodes)
 
 	for index := 0; index < len(intCodes); index++ {
 		if intCodes[index] == 99 {
 			break
 		}
 
-		codeArray := intCodes[index : index+4]
-		currentCode := codeArray[0]
-		indexA := codeArray[1]
-		indexB := codeArray[2]
-		positionToUpdate := codeArray[3]
+		currentCode := intCodes[index]
+		indexA := intCodes[index+1]
+		indexB := intCodes[index+2]
+		positionToUpdate := intCodes[index+3]
 
 		valueA := intCodes[indexA]
-		fmt.Println("valueA", valueA)
 		valueB := intCodes[indexB]
-		fmt.Println("valueB", valueB)
+
+		if currentCode == 1 || currentCode == 2 {
+			fmt.Println("*************")
+			fmt.Println("indexA", indexA)
+			fmt.Println("indexB", indexB)
+			fmt.Println("valueA", valueA)
+			fmt.Println("valueB", valueB)
+			fmt.Println("positionToUpdate", positionToUpdate)
+			fmt.Println("value at that position", intCodes[positionToUpdate])
+		}
 
 		switch currentCode {
 		case 1:
-			fmt.Println("codeArray", codeArray)
 			fmt.Println(fmt.Sprintf("%d + %d = %d", valueA, valueB, valueA+valueB))
 			intCodes[positionToUpdate] = valueA + valueB
 			index += 4
 		case 2:
-			fmt.Println("codeArray", codeArray)
 			fmt.Println(fmt.Sprintf("%d * %d = %d", valueA, valueB, valueA*valueB))
 			intCodes[positionToUpdate] = valueA * valueB
 			index += 4
 		}
 	}
+
 	fmt.Println(intCodes)
 	fmt.Println("=====>", intCodes[0])
 }
